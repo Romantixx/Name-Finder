182ecd4a58a0a77e178b5f78de3025f5ff1793a3
Roman Cabada-Macedo <Roman Cabada-Macedo@ROMAN-MAYASPC>
2025-05-04 23:15:16 -0700

    Add project files.

---
+++ Project 2/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnGet = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.lstNames = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(102, 78)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(156, 20)
+        Me.txtName.TabIndex = 0
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(102, 117)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnGet
+        '
+        Me.btnGet.Location = New System.Drawing.Point(102, 157)
+        Me.btnGet.Name = "btnGet"
+        Me.btnGet.Size = New System.Drawing.Size(75, 23)
+        Me.btnGet.TabIndex = 2
+        Me.btnGet.Text = "Get"
+        Me.btnGet.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(183, 117)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.TabIndex = 3
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(183, 157)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearch.TabIndex = 4
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'lstNames
+        '
+        Me.lstNames.FormattingEnabled = True
+        Me.lstNames.Location = New System.Drawing.Point(410, 78)
+        Me.lstNames.Name = "lstNames"
+        Me.lstNames.Size = New System.Drawing.Size(144, 147)
+        Me.lstNames.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(775, 554)
+        Me.Controls.Add(Me.lstNames)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnGet)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtName)
+        Me.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Name = "Form1"
+        Me.Text = "Name Finder"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnGet As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents lstNames As ListBox
+End Class
---
+++ Project 2/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form1
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim inputName As String = txtName.Text.Trim()
+
+        If inputName.Count(Function(c) c = " "c) = 1 Then
+            Dim formattedName As String = FormatName(inputName)
+            lstNames.Items.Add(formattedName)
+            txtName.Clear()
+        Else
+            MessageBox.Show("Enter name as: FirstName LastName", "Invalid Format")
+        End If
+    End Sub
+
+    Private Function FormatName(fullName As String) As String
+        Dim parts() As String = fullName.Split(" "c)
+        Dim first As String = parts(0)
+        Dim last As String = parts(1)
+        Return last & ", " & first
+    End Function
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        If lstNames.SelectedIndex >= 0 Then
+            lstNames.Items.RemoveAt(lstNames.SelectedIndex)
+        Else
+            MessageBox.Show("Select a name to delete.")
+        End If
+    End Sub
+
+    Private Sub btnGet_Click(sender As Object, e As EventArgs) Handles btnGet.Click
+        If lstNames.SelectedIndex >= 0 Then
+            Dim selectedName As String = lstNames.SelectedItem.ToString()
+            Dim normalName As String = ConvertToFirstLast(selectedName)
+            MessageBox.Show("Original Name: " & normalName)
+        Else
+            MessageBox.Show("Select a name to get.")
+        End If
+    End Sub
+
+
+    Private Function ConvertToFirstLast(formattedName As String) As String
+        Dim parts() As String = formattedName.Split(","c)
+        Dim last As String = parts(0).Trim()
+        Dim first As String = parts(1).Trim()
+        Return first & " " & last
+    End Function
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        Dim inputLastName As String = InputBox("Enter last name to search:")
+
+        If inputLastName = "" Then Exit Sub
+
+        Dim found As Boolean = SearchLastName(inputLastName.Trim())
+        If found Then
+            MessageBox.Show("Last name FOUND in the list.", "Search Result")
+        Else
+            MessageBox.Show("Last name NOT found.", "Search Result")
+        End If
+    End Sub
+
+    Private Function SearchLastName(lastName As String) As Boolean
+        For Each item As String In lstNames.Items
+            Dim currentLastName As String = item.Split(","c)(0).Trim()
+            If String.Equals(currentLastName, lastName, StringComparison.OrdinalIgnoreCase) Then
+                Return True
+            End If
+        Next
+        Return False
+    End Function
+End Class
